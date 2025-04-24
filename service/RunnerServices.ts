af04df1c62c73c18deab4fb14ed44135214ade11
Jorge Andres Marles Florez <jamarlesf@gmail.com>
2025-04-23 23:08:52 -0500

    Update RunnerServices.ts

--- service/RunnerServices.ts
+++ service/RunnerServices.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { CodeExecutor } from '../utils/CodeExecutor';
+import { ROOT_DIR } from "../config";
 
 export const run = async (req: Request, res: Response) => {
     try {
@@ -20,7 +21,7 @@ export const run = async (req: Request, res: Response) => {
             return res.status(400).json({ message: "The code file don't exits in the request" });
         }
         try {
-            const tempFilePath = `./${req.file.path}`;
+            const tempFilePath = path.join(ROOT_DIR, req.file.path);
             const filename = req.file.filename;
             const codeExecutor = new CodeExecutor();
             const result = await codeExecutor.executeCode({ problem_id: problem_id, language: language, timeout: timeout, memoryLimit: memoryLimit, filename: filename, tempFilePath: tempFilePath });
